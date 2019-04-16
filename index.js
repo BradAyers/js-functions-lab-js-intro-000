b78bd2e3c043d38ae5059453713c95d7b914e118
Brad Ayers <brad@bradayers.com>
2019-04-16 01:36:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
-  `Happy ${holiday}, ${name}!`;
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
-  `It's ${days} days until ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`;
 }
